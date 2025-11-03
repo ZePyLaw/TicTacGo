42e40f6525b17d59f58805306efd2538f680062b
Jérémy Prin <jeremy.prin@hes-so.ch>
2025-11-03 10:35:38 +0100

    ⬆️ Update dependencies with last version and Go 1.25.3

--- main.go
+++ main.go
@@ -4,12 +4,6 @@ import (
 	"bytes"
 	"embed"
 	"fmt"
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
-	"github.com/hajimehoshi/ebiten/v2/text"
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/opentype"
 	"image"
 	"image/color"
 	_ "image/png"
@@ -17,6 +11,11 @@ import (
 	"math/rand"
 	"os"
 	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
 
 const (
@@ -31,8 +30,8 @@ const (
 var imageFS embed.FS
 
 var (
-	normalText  font.Face
-	bigText     font.Face
+	normalText  text.Face
+	bigText     text.Face
 	boardImage  *ebiten.Image
 	symbolImage *ebiten.Image
 	textImage   = ebiten.NewImage(sWidth, sWidth)
@@ -102,7 +101,11 @@ func keyChangeColor(key ebiten.Key, screen *ebiten.Image) {
 			msgText = fmt.Sprintf("RESETING...")
 			colorText = color.RGBA{R: colorChange, G: 255, B: 255, A: 255}
 		}
-		text.Draw(screen, msgText, normalText, sWidth/2, sHeight-30, colorText)
+
+		msgTextOptions := &text.DrawOptions{}
+		msgTextOptions.GeoM.Translate(sWidth/2, sHeight-30)
+		msgTextOptions.ColorScale.ScaleWithColor(colorText)
+		text.Draw(screen, msgText, normalText, msgTextOptions)
 	}
 }
 
@@ -112,19 +115,35 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(gameImage, nil)
 	mx, my := ebiten.CursorPosition()
 
-	msgFPS := fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS())
-	text.Draw(screen, msgFPS, normalText, 0, sHeight-30, color.White)
+	msgFPS := fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.ActualTPS(), ebiten.ActualFPS())
+	op := &text.DrawOptions{}
+	op.GeoM.Translate(0, sHeight-60)
+	op.ColorScale.ScaleWithColor(color.White)
+	op.LayoutOptions.LineSpacing = 15
+	text.Draw(screen, msgFPS, normalText, op)
 
 	keyChangeColor(ebiten.KeyEscape, screen)
 	keyChangeColor(ebiten.KeyR, screen)
 	msgOX := fmt.Sprintf("O: %v | X: %v", g.pointsO, g.pointsX)
-	text.Draw(screen, msgOX, normalText, sWidth/2, sHeight-5, color.White)
+	msgOXOptions := &text.DrawOptions{}
+	msgOXOptions.GeoM.Translate(sWidth/2, sHeight-30)
+	msgOXOptions.LayoutOptions.PrimaryAlign = text.AlignCenter
+	msgOXOptions.ColorScale.ScaleWithColor(color.White)
+	text.Draw(screen, msgOX, normalText, msgOXOptions)
 	if g.win != "" {
 		msgWin := fmt.Sprintf("%v wins!", g.win)
-		text.Draw(screen, msgWin, bigText, 70, 200, color.RGBA{G: 50, B: 200, A: 255})
+		msgWinOptions := &text.DrawOptions{}
+		msgWinOptions.GeoM.Translate(sWidth/2, sHeight/2)
+		msgWinOptions.LayoutOptions.PrimaryAlign = text.AlignCenter
+		msgWinOptions.LayoutOptions.SecondaryAlign = text.AlignCenter
+		msgWinOptions.ColorScale.ScaleWithColor(color.White)
+		text.Draw(screen, msgWin, bigText, msgWinOptions)
 	}
 	msg := fmt.Sprintf("%v", g.playing)
-	text.Draw(screen, msg, normalText, mx, my, color.RGBA{G: 255, A: 255})
+	msgOptions := &text.DrawOptions{}
+	msgOptions.GeoM.Translate(float64(mx - 15), float64(my - 15))
+	msgOptions.ColorScale.ScaleWithColor(color.White)
+	text.Draw(screen, msg, normalText, msgOptions)
 }
 
 func (g *Game) DrawSymbol(x, y int, sym string) {
@@ -221,25 +240,18 @@ func (g *Game) ResetPoints() {
 }
 
 func init() {
-	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	tt, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
 	if err != nil {
 		log.Fatal(err)
 	}
-	normalText, err = opentype.NewFace(tt, &opentype.FaceOptions{
-		Size:    fontSize,
-		DPI:     dpi,
-		Hinting: font.HintingFull,
-	})
-	if err != nil {
-		log.Fatal(err)
+	normalText = &text.GoTextFace{
+		Source: tt,
+		Size:   fontSize,
 	}
-	bigText, err = opentype.NewFace(tt, &opentype.FaceOptions{
-		Size:    bigFontSize,
-		DPI:     dpi,
-		Hinting: font.HintingFull,
-	})
-	if err != nil {
-		log.Fatal(err)
+
+	bigText = &text.GoTextFace{
+		Source: tt,
+		Size:   bigFontSize,
 	}
 }
 
